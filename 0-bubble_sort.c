86328b7c985cd30c85c6338fed8fb067a98082b0
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-12-19 12:04:53 -0800

    Betty fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,27 +8,27 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    int swapped, m, n;
+	int swapped, m, n;
 
-    for (m = 0; m < (int)size - 1; m++)
-    {
-        swapped = 0;
+	for (m = 0; m < (int)size - 1; m++)
+	{
+		swapped = 0;
 
-        for (n = 0; n < (int)size - m - 1; n++)
-        {
-            if (array[n] > array[n + 1])
-            {
-                int temp = array[n];
+		for (n = 0; n < (int)size - m - 1; n++)
+		{
+			if (array[n] > array[n + 1])
+			{
+				int temp = array[n];
 
-                array[n] = array[n + 1];
-                array[n + 1] = temp;
-                swapped = 1;
-                /* Print the current state of the array after each pass */
-                print_array(array, size);
-            }
-        }
-        /* If no two elements were swapped by inner loop, then break */
-        if (swapped == 0)
-            break;
-    }
+				array[n] = array[n + 1];
+				array[n + 1] = temp;
+				swapped = 1;
+/* Print the current state of the array after each pass */
+				print_array(array, size);
+			}
+		}
+/* If no two elements were swapped by inner loop, then break */
+		if (swapped == 0)
+			break;
+	}
 }
